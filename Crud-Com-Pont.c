9951dc48ec9743c649403bd5f9c68c869bc7c48b
Walyson Henrique Ozório Sobrinho <20231STADS005@cefetes.br>
2023-09-27 13:34:15 -0300

    Alocacao

---
+++ Crud-Com-Pont.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void Cadastro()
+{
+    
+}
+
+
+
+
+
+
+int main(void)
+{
+
+
+
+
+
+
+    return 0;
+}
---
+++ Ponteiros-E-Vetores.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main (void)
+{
+int vetor[10];
+int *pvetor;
+
+pvetor = vetor;
+
+printf("Variavel apontando para o endereco usando &vetor[0] : %x\nVariavel pvetor apontando endereco usando pvetor : %x", &vetor[0], pvetor);
+
+
+    return 0;
+}
