12dc1788826f0afe2f4677716c8e9539ced56cf5
Walyson Henrique Ozório Sobrinho <20231STADS005@cefetes.br>
2023-09-27 13:36:23 -0300

    testando

--- Ponteiros-E-Vetores.c
+++ Ponteiros-E-Vetores.c
@@ -10,6 +10,6 @@ pvetor = vetor;
 
 printf("Variavel apontando para o endereco usando &vetor[0] : %x\nVariavel pvetor apontando endereco usando pvetor : %x", &vetor[0], pvetor);
 
-
+//Teste
     return 0;
 }
